0997b309a11bd483e13fd192f2c6ef6b4daea0b9
= <quoc_pham@msn.com>
2019-01-13 13:12:05 -0600

    Fully implement reducer and connected counter component to redux.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,41 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+  state = {
+    counter: 0
+  };
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
+  counterChangedHandler = (action, value) => {
+    switch (action) {
+      case "inc":
+        this.setState(prevState => {
+          return { counter: prevState.counter + 1 };
+        });
+        break;
+      case "dec":
+        this.setState(prevState => {
+          return { counter: prevState.counter - 1 };
+        });
+        break;
+      case "add":
+        this.setState(prevState => {
+          return { counter: prevState.counter + value };
+        });
+        break;
+      case "sub":
+        this.setState(prevState => {
+          return { counter: prevState.counter - value };
+        });
+        break;
+      default:
+        this.setState(prevState => {
+          return { counter: prevState.counter };
+        });
     }
+  };
 
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl label="Increment" clicked={this.props.incHandler} />
+        <CounterControl label="Decrement" clicked={this.props.decHandler} />
+        <CounterControl label="Add 5" clicked={this.props.addHandler} />
+        <CounterControl label="Subtract 5" clicked={this.props.subHandler} />
+      </div>
+    );
+  }
 }
 
-export default Counter;
+// A function that maps state properties to component props.
+// In example below, state.counter field is mapped to ctr field
+// in props of the Counter component.
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    incHandler: () => dispatch({ type: "INCREMENT" }),
+    decHandler: () => dispatch({ type: "DECREMENT" }),
+    addHandler: () => dispatch({ type: "ADD_5", value: 5 }),
+    subHandler: () => dispatch({ type: "SUBTRACT_5", value: 5 })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,18 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DECREMENT") {
+    return { counter: state.counter - 1 };
+  }
+  if (action.type === "ADD_5") {
+    return { counter: state.counter + action.value };
+  }
+  if (action.type === "SUBTRACT_5") {
+    return { counter: state.counter - action.value };
+  }
   return state;
 };
 
